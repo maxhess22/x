129d33af920116c88e564cdda63f900dc78f9e8d
maxhess22 <ma.hessalvarez@duocuc.cl>
2021-06-21 02:36:02 -0400

    conexion de urls de el core

--- Aplicación_web_Evaluacion_3/Evaluacion3/core/urls.py
+++ Aplicación_web_Evaluacion_3/Evaluacion3/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home,form
+
 
 
 urlpatterns = [
     path('', home,name="index"),
+    path('formulario', form, name="forma")
+   
 ]
--- Aplicación_web_Evaluacion_3/Evaluacion3/core/views.py
+++ Aplicación_web_Evaluacion_3/Evaluacion3/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
+    return render(request,'core/index.html'),
 
-    return render(request, 'core/index.html')
+
+
+def form(request):
+    return render(request,'core/Formulario.html')
+
+
 
 
