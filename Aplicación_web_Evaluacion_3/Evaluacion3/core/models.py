873dcd4840d16750ce594872626abad64724440e
maxhess22 <ma.hessalvarez@duocuc.cl>
2021-06-22 07:14:30 -0400

    Arreglo de base de datos y modelos

--- Aplicación_web_Evaluacion_3/Evaluacion3/core/models.py
+++ Aplicación_web_Evaluacion_3/Evaluacion3/core/models.py
@@ -3,12 +3,15 @@ from django.db import models
 
 
 class categoria_libro(models.Model):
+    id_categoria= models.IntegerField(primary_key=True)
     nombre_categoria = models.CharField(max_length=50)
 
   
 
 
 class libro(models.Model):
+    id_libro = models.IntegerField(primary_key=True)
     nombre_libro = models.CharField(max_length=50)
     autor_libro = models.CharField(max_length=50)
     descripcion_libro = models.CharField(max_length=50)
+    categoria_libro = models.ForeignKey( categoria_libro, on_delete=models.CASCADE)
