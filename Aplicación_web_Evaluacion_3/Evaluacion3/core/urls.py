320067398e27a7f008cd7c27e7630705e953d8b7
maxhess22 <ma.hessalvarez@duocuc.cl>
2021-08-06 15:38:24 -0400

    incorporacion de ambiente virtual

--- Aplicación_web_Evaluacion_3/Evaluacion3/core/urls.py
+++ Aplicación_web_Evaluacion_3/Evaluacion3/core/urls.py
@@ -5,7 +5,7 @@ from .views import home,form_libro,visualizacion, listresults
 
 
 urlpatterns = [
-    path('home', home,name="home"),
+    path('', home,name="home"),
     path("Añadir", form_libro, name="añadir"),
     path("visualizar", visualizacion, name="visualizar1"),
     path("descarga", listresults, name="descarga"),
